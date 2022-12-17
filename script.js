022dbb1630828e72fb1e453908e82de0d6ae7383
Raian <96938727+raian30@users.noreply.github.com>
2022-12-17 14:23:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const doc = document.querySelector("body")
 let checked;
 const darkon = () => {
     checked = checked !== true;
-
+    let logo = doc.querySelector(".navlogo img")
     if (checked === true) {
         document.documentElement.style
             .setProperty('--background', '#d4d2d2');
         document.documentElement.style
             .setProperty('--text', '#2a2a2a');
+        logo.src="/slike/logoDark.png";
+
     } else {
         document.documentElement.style
             .setProperty('--background', '#2a2a2a');
         document.documentElement.style
             .setProperty('--text', '#d4d2d2');
+        logo.src="/slike/logoLight.png";
     }
 }
 
